c0401203467e5ad016efdf6d6cb99eb89fbd56cb
lsw <lsw5244@naver.com>
2020-10-10 17:16:40 +0900

    버섯 몬스터 임포트, 이동 로직 구현
    버섯 몬스터 이동 구현 및 임포트

---
+++ Assets/Assets_LSW/Baby Mushroom - Pixel Art/Demo/BabyMushroom.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BabyMushroom : MonoBehaviour {
+
+    [SerializeField] float      m_speed = 1.4f;
+    [SerializeField] GameObject m_gas;
+
+    private Animator            m_animator;
+    private Rigidbody2D         m_body2d;
+    private Transform           m_gasSpawnLocation;
+
+    // Use this for initialization
+    void Start () {
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_gasSpawnLocation = transform.Find("GasSpawnLocation");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // -- Handle input and movement --
+        float inputX = Input.GetAxis("Horizontal");
+
+        // Swap direction of sprite depending on walk direction
+        if (inputX > 0)
+            GetComponent<SpriteRenderer>().flipX = false;
+        else if (inputX < 0)
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        // Move
+        m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+
+        // -- Handle Animations --
+        //Death
+        if (Input.GetKeyDown("e")) {
+            m_animator.SetTrigger("Death");
+        }
+            
+        //Hurt
+        else if (Input.GetKeyDown("q"))
+            m_animator.SetTrigger("Hurt");
+
+        //Attack
+        else if(Input.GetMouseButtonDown(0)) {
+            m_animator.SetTrigger("Attack");
+        }
+
+        //Run
+        else if (Mathf.Abs(inputX) > Mathf.Epsilon)
+            m_animator.SetInteger("AnimState", 1);
+
+        //Idle
+        else
+            m_animator.SetInteger("AnimState", 0);
+    }
+
+    void spawnGas() {
+        if (m_gas != null)
+            Instantiate(m_gas, m_gasSpawnLocation);
+    }
+}
+
+
---
+++ Assets/Assets_LSW/Scripts/Monster_Mushroom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// TODO : 애니메이션 넣기
+public class Monster_Mushroom : MonoBehaviour
+{
+
+    Rigidbody2D rigi;
+    int moveDir = 1;
+    [SerializeField] float speed = 10f;
+
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        CheckFloor();
+        rigi.velocity = new Vector2(moveDir * speed, rigi.velocity.y);
+    }
+
+    void CheckFloor()
+    {
+        // 바닥 체크
+        Vector2 frontVector = new Vector2(rigi.position.x + moveDir * 0.8f, rigi.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(frontVector, Vector3.down, 1);
+        if (hit.collider == null)
+        {
+            moveDir *= -1;
+            transform.Rotate(0, 180, 0);
+            StopAllCoroutines();
+            StartCoroutine("ChangeDir");
+        }
+    }
+
+    IEnumerator ChangeDir()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        moveDir = Random.Range(-1, 2);
+        StartCoroutine("ChangeDir");
+    }
+}
