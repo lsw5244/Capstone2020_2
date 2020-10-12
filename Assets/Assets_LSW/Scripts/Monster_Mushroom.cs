801a7a1ed72e20315f1522bc1af7321e659f7d36
lsw <lsw5244@naver.com>
2020-10-12 17:13:19 +0900

    버섯 몬스터 공격 로직 임시 제작
    클릭 시 몬스터 공격 애니메이션 출력되도록 제작 및 공격 로직 구현
    버섯 몬스터 에셋의 LSWCustome폴더에 애니메이션 및 애니메이터 구현

--- Assets/Assets_LSW/Scripts/Monster_Mushroom.cs
+++ Assets/Assets_LSW/Scripts/Monster_Mushroom.cs
@@ -1,25 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// TODO : 애니메이션 넣기
+// TODO : HP, 사망 모션 추가하기
+// FIX : 공격 애니메이션 때 가스 나오는 위치 조절
 public class Monster_Mushroom : MonoBehaviour
 {
 
     Rigidbody2D rigi;
+    Animator ani;
+
     int moveDir = 1;
     [SerializeField] float speed = 10f;
     float hp = 3;
 
+    string attack = "Mushroom_Purple_Attack 1";
+    string die = "Mushroom_Purple_Death 1";
+    string hurt = "Mushroom_Purple_Hurt 1";
+    string walk = "Mushroom_Purple_Walk 1";
+    string currentState;
+
+    bool isAttack = false;
+
     void Start()
     {
         rigi = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
         StartCoroutine("ChangeDir");
+        ChangeAnimation(walk);
     }
 
     void Update()
     {
         CheckFloor();
         rigi.velocity = new Vector2(moveDir * speed, rigi.velocity.y);
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(!isAttack)
+            {
+                StopAllCoroutines();
+                StartCoroutine("Attack");
+            }        
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        isAttack = true;
+        ChangeAnimation(attack);
+        int temp = moveDir;
+        moveDir = 0;
+        yield return new WaitForSeconds(1.0f);
+        ChangeAnimation(walk);
+        moveDir = temp;
+        StartCoroutine("ChangeDir");
+        isAttack = false;
+    }
+
+    IEnumerator Die()
+    {
+        ani.Play(die);
+        moveDir = 0;
+        yield return new WaitForSeconds(2.0f);
+        Destroy(GetComponentInParent<Transform>().gameObject);
     }
 
     void CheckFloor()
@@ -39,7 +81,7 @@ public class Monster_Mushroom : MonoBehaviour
     IEnumerator ChangeDir()
     {
         int temp = moveDir;
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(2.0f);
 
        // moveDir = Random.Range(-1, 2);
         moveDir = Random.Range(0, 2) < 1 ? -1 : 1;
@@ -49,5 +91,13 @@ public class Monster_Mushroom : MonoBehaviour
             transform.Rotate(0, 180, 0);
         }
         StartCoroutine("ChangeDir");
+    }
+
+    void ChangeAnimation(string State)
+    {
+        if (currentState == State)
+            return;
+        currentState = State;
+        ani.Play(State);
     }
 }
