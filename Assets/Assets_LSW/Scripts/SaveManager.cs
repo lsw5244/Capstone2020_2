2fa24334656fe50bda81f23752cffe433b15f52e
lsw <lsw5244@naver.com>
2020-10-28 23:57:17 +0900

    SaveManager.cs제작
    SaveManager.cs제작 및 변수 초기화
    SaveManager.cs의 동작 확인을 위한 Temp.cs제작

---
+++ Assets/Assets_LSW/Scripts/SaveManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    static public SaveManager saveManager;
+    private float str = 10f;
+    public float Str
+    {
+        get { return str; } 
+    }
+    public void PlusStr(float add)
+    {
+        str += add;
+        //Debug.Log(str);
+    }
+
+    float dex;
+    float def;
+    float luk;
+    float mov;
+    float crt;
+
+    private void Awake()
+    {
+        if(saveManager == null)
+        {
+            saveManager = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Assets_LSW/Scripts/Temp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(SaveManager.saveManager.Str);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            SaveManager.saveManager.PlusStr(5f);
+        }
+    }
+}
