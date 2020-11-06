f3de7f620f09031b87eb65fe40c05a773f59849f
hangobles <wnsdl9703@gmail.com>
2020-11-06 22:43:51 +0900

    공격기능 추가
    공격(표창날리기 애니메이션)
    표창의 애니메이션과 이동

--- Assets/Assets_HSJ/Script/Player.cs
+++ Assets/Assets_HSJ/Script/Player.cs
@@ -6,15 +6,59 @@ public class Player : PlayerState
 {
     public Joystick joystick;
     Rigidbody2D rb;
-   public bool isground =false;
+    public bool isground =false;
+    public float walkcooltime;
+    //사운드
+    AudioSource audioSource;
+    AudioClip jumpSound;
+    AudioClip walkSound;
+    //애니메이션
+    string playerAnimationState;
+    Animator animator;
+
+    string idle = "HeroKnight_Idle";
+    string jump = "HeroKnight_Jump";
+    string jumpToFall = "HeroKnight_Fall";
+    string run = "HeroKnight_Run";
+    string attack = "HeroKnight_Attack1";
+    string attack2 = "HeroKnight_Attack2";
+    string attack3 = "HeroKnight_Attack3";
+    string death = "HeroKnight_Death";
+    string hurt = "HeroKnight_Hurt";
+
+    //공격
+    public GameObject attackCollider;
+    public bool isAttack;
+    public float attackcooltime;
+    public GameObject attackRange;
+    public int combo;
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
         if (HP<=0) {
-            //죽으면 게임오버? 다시하기?
+            changeAnimation(death);
+        }
+        if (isground && !isAttack)
+        {
+            if (joystick.joystickVector.x != 0) {
+                if (walkcooltime > 2f) {
+                    audioSource.PlayOneShot(walkSound, 1);
+                    walkcooltime = 0;
+                }
+                changeAnimation(run);
+            }
+            else
+            {
+                changeAnimation(idle);
+            }
+            if (walkcooltime <= 2) {
+                walkcooltime += 0.1f;
+            }
         }
     }
     private void FixedUpdate()
@@ -30,17 +74,63 @@ public class Player : PlayerState
     public void Jump() {
         if (rb.velocity.y>=0&&isground==true) {
             rb.AddForce(Vector2.up * jumpPower);
+            changeAnimation("jump");
+            audioSource.PlayOneShot(jumpSound);
         }
     }
     //공격
-    public void Attack() {
-        //공격했을 시 속도를 0으로 하여 움직이지 못하게 시도해봄
-        rb.velocity = new Vector2(0, rb.velocity.y);
-
-
+    public void Attack()
+    {
+        if (attackcooltime >= 0.5f) {
+            isAttack = true;
+            attackcooltime = 0;
+            attackRange.SetActive(true);
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            if (combo == 0)
+            {
+                changeAnimation(attack);
+                combo++;
+            }
+            else if (combo == 1)
+            {
+                changeAnimation(attack2);
+                combo++;
+            }
+            else {
+                changeAnimation(attack3);
+                combo = 0;
+            }
+            Invoke("attackcomplete", 0.4f);
+        }
+        else
+        {
+            return;
+        }
+    }
+    void attackDelay()
+    {
+        isAttack = false;
+        attackCollider.SetActive(false);
     }
     //피격
     public void Hit() {
+        changeAnimation(hurt);
+        HP--;
+    }
 
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "MONSTER") {
+            Hit();
+        }
+    }
+    void changeAnimation(string state)
+    {
+        if (state == playerAnimationState)
+        {
+            return;
+        }
+        animator.Play(state);
+        playerAnimationState = state;
     }
 }
