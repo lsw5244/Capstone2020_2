9f6226c7e2d5f6a9cf54242cffc72f12cdafb54a
lsw5244 <33677312+lsw5244@users.noreply.github.com>
2020-11-08 13:43:39 +0900

    Save manager (#7)
    * SaveManager.cs제작
    
    SaveManager.cs제작 및 변수 초기화
    SaveManager.cs의 동작 확인을 위한 Temp.cs제작
    
    * SaveManager.cs 수정
    
    SaveManager.cs에 각 스텟들의 getter와 스탯 추가 메소드 추가
    
    * SaveManager.cs수정, StrItem.cs추가
    
    SaveManager.cs가 Str만 저장하도록 변경, ResetStatus함수 추가, 기본 50
    Str을 올려 줄 아이템에 들어 갈 StrItem.cs추가

---
+++ Assets/Assets_LSW/Scripts/StrItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrItem : MonoBehaviour
+{
+    public float str;
+    private void OnCollisionEnter(Collision coll)
+    {
+        if(coll.transform.tag == "PLAYER")
+        {
+            SaveManager.instance.AddStr(str);
+            Destroy(this.gameObject);
+        }
+    }
+}
