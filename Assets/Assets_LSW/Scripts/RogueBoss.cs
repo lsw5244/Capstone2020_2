0ef235ad86b6a42c08f74b98fd371a507f3caf7c
lsw <lsw5244@naver.com>
2020-11-09 14:16:59 +0900

    PixelHeroesPack에셋 임포트, RogueBoss.cs추가
    보스 몬스터Rogue에 들어갈 RogueBoss.cs추가

---
+++ Assets/Assets_LSW/Scripts/RogueBoss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RogueBoss : MonoBehaviour
+{
+    private Rigidbody2D rigi;
+    private Animator ani;
+
+    [SerializeField] float speed = 10f;
+    private int moveDir = 1; // 처음 이동은 오른쪽으로 이동함
+    [SerializeField] float maxHP = 100;
+    private float currentHP;
+
+    void Start()
+    {
+        currentHP = maxHP;
+        rigi = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        CheckFloor();
+        rigi.velocity = new Vector2(moveDir * speed, rigi.velocity.y);
+    }
+    void CheckFloor()
+    {
+        Vector2 frontVector = new Vector2(rigi.position.x + moveDir * 1f, rigi.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(frontVector, Vector3.down, 1);
+        if (hit.collider == null)
+        {
+            moveDir *= -1;
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}
