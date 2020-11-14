dbec995fc9d0bb2cd1917a73e77784c351931ea9
lsw <lsw5244@naver.com>
2020-11-14 20:43:53 +0900

    RogueBoss.cs 수정
    RogueBoss.cs가 들어간 몬스터가 플래이어를 따라오도록 구현

--- Assets/Assets_LSW/Scripts/RogueBoss.cs
+++ Assets/Assets_LSW/Scripts/RogueBoss.cs
@@ -11,17 +11,22 @@ public class RogueBoss : MonoBehaviour
     private int moveDir = 1; // 처음 이동은 오른쪽으로 이동함
     [SerializeField] float maxHP = 100;
     private float currentHP;
+    [SerializeField] Transform playerTr;
 
     void Start()
     {
         currentHP = maxHP;
         rigi = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
+        // 걷도록 애니메이션 변경
+        ani.SetBool("Grounded", true);
+        ani.SetInteger("AnimState", 1);
     }
 
     void Update()
     {
-        CheckFloor();
+        //CheckFloor();
+        CheckPlayerPosition();
         rigi.velocity = new Vector2(moveDir * speed, rigi.velocity.y);
     }
     void CheckFloor()
@@ -31,6 +36,24 @@ public class RogueBoss : MonoBehaviour
         if (hit.collider == null)
         {
             moveDir *= -1;
+            transform.Rotate(0, 180, 0);
+        }
+    }
+    void CheckPlayerPosition()
+    {
+        // 플레이어가 오른쪽에 있을 때
+        if (playerTr.position.x > transform.position.x)
+        {
+            if (moveDir == 1)
+                return;
+            moveDir = 1;
+            transform.Rotate(0, 180, 0);
+        }
+        else if (playerTr.position.x < transform.position.x) // 플레이어가 왼쪽에 있을 때
+        {
+            if (moveDir == -1)
+                return;
+            moveDir = -1;
             transform.Rotate(0, 180, 0);
         }
     }
